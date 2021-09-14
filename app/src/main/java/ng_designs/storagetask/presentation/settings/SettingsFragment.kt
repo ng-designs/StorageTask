bad16820ff526b6a8f00db61beb9ca75981b48aa
ng-designs <nafis.gimranov@gmail.com>
2021-09-14 11:19:55 +0500

    add: Navigation Component, NavGraph

--- app/src/main/java/ng_designs/storagetask/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/ng_designs/storagetask/presentation/settings/SettingsFragment.kt
@@ -1,10 +1,19 @@
 package ng_designs.storagetask.presentation.settings
 
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import androidx.preference.PreferenceFragmentCompat
 import ng_designs.storagetask.R
+import ng_designs.storagetask.presentation.main.MainViewModel
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    private lateinit var viewModel: SettingsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
